16bc8e4b7c7ec296fdc16c91c74d790e1ecd3e56
Documenter.jl <documenter@juliadocs.github.io>
2022-02-02 10:35:46 +0000

    build based on 0138edf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SymmetricSquares","category":"page"},{"location":"#SymmetricSquares","page":"Home","title":"SymmetricSquares","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SymmetricSquares.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SymmetricSquares]","category":"page"}]
+}
