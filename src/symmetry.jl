d431e7a41be5cede5a050b3ed10a57b3e8a9a48d
Elias Kempf <34234056+icetube23@users.noreply.github.com>
2022-02-02 15:31:45 +0100

    Implemented function composition as group operation for the symmetry group.

--- src/symmetry.jl
+++ src/symmetry.jl
@@ -6,12 +6,81 @@ fy(x::AbstractMatrix) = reverse(x, dims=2)
 r180(x::AbstractMatrix) = reverse(x)
 
 # compound implementations
-r90(x::AbstractMatrix) = fy(tr(x)) # mini optimization to fy(x') ?
+r90(x::AbstractMatrix) = fy(x') # fy(x') is slightly more efficient than fy(tr(x))
 r270(x::AbstractMatrix) = tr(fy(x))
-trr(x::AbstractMatrix) = r90(fy(x))
+trr(x::AbstractMatrix) = r180(x') # r180(x') is slightly more efficient than r180(tr(x))
 
-∘(::Val{fy}, ::Val{tr}) = r90
-∘(::Val{tr}, ::Val{fx}) = r270
-∘(::Val{r180}, ::Val{fy}) = fx
-∘(::Val{r90}, ::Val{fy}) = trr
-∘(f::Function, g::Function) = ∘(Val(f), Val(g))
+# group operation for elements of the symmetry group
+Base.:∘(::Val{e}, ::Val{e}) = e
+Base.:∘(::Val{e}, ::Val{fx}) = fx
+Base.:∘(::Val{e}, ::Val{fy}) = fy
+Base.:∘(::Val{e}, ::Val{tr}) = tr
+Base.:∘(::Val{e}, ::Val{trr}) = trr
+Base.:∘(::Val{e}, ::Val{r90}) = r90
+Base.:∘(::Val{e}, ::Val{r180}) = r180
+Base.:∘(::Val{e}, ::Val{r270}) = r270
+
+Base.:∘(::Val{fx}, ::Val{e}) = fx
+Base.:∘(::Val{fx}, ::Val{fx}) = e
+Base.:∘(::Val{fx}, ::Val{fy}) = r180
+Base.:∘(::Val{fx}, ::Val{tr}) = r270
+Base.:∘(::Val{fx}, ::Val{trr}) = r90
+Base.:∘(::Val{fx}, ::Val{r90}) = trr
+Base.:∘(::Val{fx}, ::Val{r180}) = fy
+Base.:∘(::Val{fx}, ::Val{r270}) = tr
+
+Base.:∘(::Val{fy}, ::Val{e}) = fy
+Base.:∘(::Val{fy}, ::Val{fx}) = r180
+Base.:∘(::Val{fy}, ::Val{fy}) = e
+Base.:∘(::Val{fy}, ::Val{tr}) = r90
+Base.:∘(::Val{fy}, ::Val{trr}) = r270
+Base.:∘(::Val{fy}, ::Val{r90}) = tr
+Base.:∘(::Val{fy}, ::Val{r180}) = fx
+Base.:∘(::Val{fy}, ::Val{r270}) = trr
+
+Base.:∘(::Val{tr}, ::Val{e}) = tr
+Base.:∘(::Val{tr}, ::Val{fx}) = r90
+Base.:∘(::Val{tr}, ::Val{fy}) = r270
+Base.:∘(::Val{tr}, ::Val{tr}) = e
+Base.:∘(::Val{tr}, ::Val{trr}) = r180
+Base.:∘(::Val{tr}, ::Val{r90}) = fx
+Base.:∘(::Val{tr}, ::Val{r180}) = trr
+Base.:∘(::Val{tr}, ::Val{r270}) = fy
+
+Base.:∘(::Val{trr}, ::Val{e}) = trr
+Base.:∘(::Val{trr}, ::Val{fx}) = r270
+Base.:∘(::Val{trr}, ::Val{fy}) = r90
+Base.:∘(::Val{trr}, ::Val{tr}) = r180
+Base.:∘(::Val{trr}, ::Val{trr}) = e
+Base.:∘(::Val{trr}, ::Val{r90}) = fy
+Base.:∘(::Val{trr}, ::Val{r180}) = tr
+Base.:∘(::Val{trr}, ::Val{r270}) = fx
+
+Base.:∘(::Val{r90}, ::Val{e}) = r90
+Base.:∘(::Val{r90}, ::Val{fx}) = tr
+Base.:∘(::Val{r90}, ::Val{fy}) = trr
+Base.:∘(::Val{r90}, ::Val{tr}) = fy
+Base.:∘(::Val{r90}, ::Val{trr}) = fx
+Base.:∘(::Val{r90}, ::Val{r90}) = r180
+Base.:∘(::Val{r90}, ::Val{r180}) = r270
+Base.:∘(::Val{r90}, ::Val{r270}) = e
+
+Base.:∘(::Val{r180}, ::Val{e}) = r180
+Base.:∘(::Val{r180}, ::Val{fx}) = fy
+Base.:∘(::Val{r180}, ::Val{fy}) = fx
+Base.:∘(::Val{r180}, ::Val{tr}) = trr
+Base.:∘(::Val{r180}, ::Val{trr}) = tr
+Base.:∘(::Val{r180}, ::Val{r90}) = r270
+Base.:∘(::Val{r180}, ::Val{r180}) = e
+Base.:∘(::Val{r180}, ::Val{r270}) = r90
+
+Base.:∘(::Val{r270}, ::Val{e}) = r270
+Base.:∘(::Val{r270}, ::Val{fx}) = trr
+Base.:∘(::Val{r270}, ::Val{fy}) = tr
+Base.:∘(::Val{r270}, ::Val{tr}) = fx
+Base.:∘(::Val{r270}, ::Val{trr}) = fy
+Base.:∘(::Val{r270}, ::Val{r90}) = e
+Base.:∘(::Val{r270}, ::Val{r180}) = r90
+Base.:∘(::Val{r270}, ::Val{r270}) = r180
+
+Base.:∘(f::Function, g::Function) = ∘(Val(f), Val(g))
--- test/symmetry.jl
+++ test/symmetry.jl
@@ -44,3 +44,83 @@ end
                    6 5 4;
                    9 8 7]
 end
+
+@testset "Group operation" begin
+    # the '∘' function acts as function composition and group operation at the same time
+    # it can be used prevent unneeded computations, e.g., (r180 ∘ r180)(m) does not actually
+    # rotate m twice by 180 degrees but directly returns e(m)
+    m = rand(10, 10)
+
+    # validate that taking these shortcuts returns the expected results
+    @test (e ∘ e)(m) == e(e(m))
+    @test (e ∘ fx)(m) == e(fx(m))
+    @test (e ∘ fy)(m) == e(fy(m))
+    @test (e ∘ tr)(m) == e(tr(m))
+    @test (e ∘ trr)(m) == e(trr(m))
+    @test (e ∘ r90)(m) == e(r90(m))
+    @test (e ∘ r180)(m) == e(r180(m))
+    @test (e ∘ r270)(m) == e(r270(m))
+
+    @test (fx ∘ e)(m) == fx(e(m))
+    @test (fx ∘ fx)(m) == fx(fx(m))
+    @test (fx ∘ fy)(m) == fx(fy(m))
+    @test (fx ∘ tr)(m) == fx(tr(m))
+    @test (fx ∘ trr)(m) == fx(trr(m))
+    @test (fx ∘ r90)(m) == fx(r90(m))
+    @test (fx ∘ r180)(m) == fx(r180(m))
+    @test (fx ∘ r270)(m) == fx(r270(m))
+
+    @test (fy ∘ e)(m) == fy(e(m))
+    @test (fy ∘ fx)(m) == fy(fx(m))
+    @test (fy ∘ fy)(m) == fy(fy(m))
+    @test (fy ∘ tr)(m) == fy(tr(m))
+    @test (fy ∘ trr)(m) == fy(trr(m))
+    @test (fy ∘ r90)(m) == fy(r90(m))
+    @test (fy ∘ r180)(m) == fy(r180(m))
+    @test (fy ∘ r270)(m) == fy(r270(m))
+
+    @test (tr ∘ e)(m) == tr(e(m))
+    @test (tr ∘ fx)(m) == tr(fx(m))
+    @test (tr ∘ fy)(m) == tr(fy(m))
+    @test (tr ∘ tr)(m) == tr(tr(m))
+    @test (tr ∘ trr)(m) == tr(trr(m))
+    @test (tr ∘ r90)(m) == tr(r90(m))
+    @test (tr ∘ r180)(m) == tr(r180(m))
+    @test (tr ∘ r270)(m) == tr(r270(m))
+
+    @test (trr ∘ e)(m) == trr(e(m))
+    @test (trr ∘ fx)(m) == trr(fx(m))
+    @test (trr ∘ fy)(m) == trr(fy(m))
+    @test (trr ∘ tr)(m) == trr(tr(m))
+    @test (trr ∘ trr)(m) == trr(trr(m))
+    @test (trr ∘ r90)(m) == trr(r90(m))
+    @test (trr ∘ r180)(m) == trr(r180(m))
+    @test (trr ∘ r270)(m) == trr(r270(m))
+
+    @test (r90 ∘ e)(m) == r90(e(m))
+    @test (r90 ∘ fx)(m) == r90(fx(m))
+    @test (r90 ∘ fy)(m) == r90(fy(m))
+    @test (r90 ∘ tr)(m) == r90(tr(m))
+    @test (r90 ∘ trr)(m) == r90(trr(m))
+    @test (r90 ∘ r90)(m) == r90(r90(m))
+    @test (r90 ∘ r180)(m) == r90(r180(m))
+    @test (r90 ∘ r270)(m) == r90(r270(m))
+
+    @test (r180 ∘ e)(m) == r180(e(m))
+    @test (r180 ∘ fx)(m) == r180(fx(m))
+    @test (r180 ∘ fy)(m) == r180(fy(m))
+    @test (r180 ∘ tr)(m) == r180(tr(m))
+    @test (r180 ∘ trr)(m) == r180(trr(m))
+    @test (r180 ∘ r90)(m) == r180(r90(m))
+    @test (r180 ∘ r180)(m) == r180(r180(m))
+    @test (r180 ∘ r270)(m) == r180(r270(m))
+
+    @test (r270 ∘ e)(m) == r270(e(m))
+    @test (r270 ∘ fx)(m) == r270(fx(m))
+    @test (r270 ∘ fy)(m) == r270(fy(m))
+    @test (r270 ∘ tr)(m) == r270(tr(m))
+    @test (r270 ∘ trr)(m) == r270(trr(m))
+    @test (r270 ∘ r90)(m) == r270(r90(m))
+    @test (r270 ∘ r180)(m) == r270(r180(m))
+    @test (r270 ∘ r270)(m) == r270(r270(m))
+end
